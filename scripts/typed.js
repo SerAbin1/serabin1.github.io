6cc28027ebbf602817034b9d4a2f523abb836028
SerAbinU1 <abinyt1234@gmail.com>
2025-03-16 07:02:53 +0000

    Add typing effect

---
+++ scripts/typed.js
@@ -0,0 +1,10 @@
+const typed = new Typed('#typed', {
+    strings: ["Cybersecurity", "Machine Learning", "Back-End Development"],
+    typeSpeed: 45,
+    backSpeed: 16,
+    startDelay: 750,
+    backDelay: 1000,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|",
+});
