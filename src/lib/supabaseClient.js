140fa3749ee20bb809960d263196b80cd82bfddf
SerAbinU1 <abinyt1234@gmail.com>
2025-07-12 10:52:08 +0000

    merge to main

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://lbmhhgnnruiyteotpajv.supabase.co"
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxibWhoZ25ucnVpeXRlb3RwYWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMTY1MjUsImV4cCI6MjA2Nzg5MjUyNX0.LUGrsZ9JXWZJ1Yn6YKYXfJC4L-hq0P6b-4QGD5-Urx4"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
