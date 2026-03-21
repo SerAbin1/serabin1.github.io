510b77957845da47da6682de4c3499dd4b1bb36f
SerAbinU1 <abinyt1234@gmail.com>
2026-03-21 15:23:46 +0530

    feat: add lastUpdated field to blogs

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const blogCollection = defineCollection({
         title: z.string(),
         description: z.string(),
         date: z.string(),
+        lastUpdated: z.string().optional(),
     }),
 });
 
