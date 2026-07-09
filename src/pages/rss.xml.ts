0f3f016ab5b9ae10cb20b1648422217bba4435b2
SerAbinU1 <abinyt1234@gmail.com>
2026-07-09 22:11:15 +0530

    feat: improve rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,27 +1,40 @@
-import rss from '@astrojs/rss';
-import { getCollection, render } from 'astro:content';
-import type { APIContext } from 'astro';
+import rss from "@astrojs/rss";
+import { getCollection, render } from "astro:content";
+import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-    const posts = await getCollection('blog');
-    const sortedPosts = posts.sort(
-        (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
-    );
+  const posts = await getCollection("blog");
+  const sortedPosts = posts.sort(
+    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
+  );
 
-    return rss({
-        title: "Abin Biju's Blog",
-        description: 'Notes on backend development, cybersecurity, and things I learn along the way.',
-        site: context.site!,
-        items: await Promise.all(sortedPosts.map(async post => {
-            const { rendered } = await render(post);
-            return {
-                title: post.data.title,
-                description: post.data.description,
-                pubDate: new Date(post.data.date),
-                link: `/blogs/${post.id}/`,
-                content: rendered.html,
-            };
-        })),
-        customData: `<language>en-us</language>`,
-    });
+  return rss({
+    title: "Abin Biju's Blog",
+    description:
+      "Notes on backend development, cybersecurity, and things I learn along the way.",
+    site: context.site!,
+    items: await Promise.all(
+      sortedPosts.map(async (post) => {
+        return {
+          title: post.data.title,
+          description: post.data.description,
+          pubDate: new Date(post.data.date),
+          link: `/blogs/${post.id}/`,
+          content: `
+<p>${post.data.description}</p>
+
+<div style="margin-top:50px;font-style:italic">
+  <strong>
+    <a href="${new URL(`/blogs/${post.id}/`, context.site)}">
+      Keep reading
+    </a>
+  </strong>
+</div>
+`,
+          author: "Abin Biju",
+        };
+      }),
+    ),
+    customData: `<language>en-us</language>`,
+  });
 }
