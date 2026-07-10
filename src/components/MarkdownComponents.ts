0e386e36ece168f4c3a68bf1ad17eb6b51fce6e8
SerAbinU1 <abinyt1234@gmail.com>
2026-07-10 14:17:20 +0530

    fix: cleanup

--- src/components/MarkdownComponents.ts
+++
@@ -1,9 +0,0 @@
-import LFUVisualizer from "./LFUVisualizer";
-import LRUVisualizer, { LRUPutVisualizer, LRUGetVisualizer } from "./LRUVisualizer";
-
-export const MarkdownComponents: Record<string, React.ComponentType<any>> = {
-    LFUVisualizer,
-    LRUVisualizer,
-    LRUPutVisualizer,
-    LRUGetVisualizer,
-};
